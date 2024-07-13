ebc3e86d87f34c6a99e224b1fd8147c5f01cdf0a
Rahul Sidar <150533760+RahulSidar08@users.noreply.github.com>
2024-07-14 00:20:02 +0700

    Add files via upload

---
+++ ProjectFiles/house.js
@@ -0,0 +1,16 @@
+// console.log("implimenting the home page ");
+
+// document.getElementById('.first').addEventListener('click' , function(){
+//     console.log("This is Home page ! ");
+// })
+
+'use strict';
+
+// const value = document.querySelector('.creation');
+
+document.getElementById('home').addEventListener('click',function()
+{
+    console.log("This is Home Page");
+})
+
+
